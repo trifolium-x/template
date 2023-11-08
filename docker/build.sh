defec9b11c15c07dc3bdd9ab2d4a3bd36a15365a
trifolium.wang <trifolium.wang@foxmail.com>
2023-11-08 19:07:45 +0800

    修改构建和运行脚本

--- docker/build.sh
+++ docker/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 mvn -skipTest=true clean package
-finalName="template"
+imageName="template"
 version="latest"
 port="8080"
-echo "构建镜像:"${finalName}:${version}
-docker stop ${finalName}
-docker rm ${finalName}
-docker rmi ${finalName}:${version}
-docker build -f ./Dockerfile --rm -t ${finalName}:${version} .
+echo "构建镜像:"${imageName}:${version}
+docker stop ${imageName}
+docker rm ${imageName}
+docker rmi ${imageName}:${version}
+docker build -f ./Dockerfile --rm -t ${imageName}:${version} .
 echo "构建完成."
 
 echo "启动中..."
-docker run --name ${finalName} -d -p ${port}:${port} \
+docker run --name ${imageName} -d -p ${port}:${port} \
 --memory-reservation=1024M --memory=1536M --oom-kill-disable=true \
--v /tmp/logs/${finalName}/${port}/logs:/app/logs
-${finalName}:${version} --spring.profiles.active=test
+-v /tmp/logs/${imageName}/${port}/logs:/app/logs
+${imageName}:${version} --spring.profiles.active=test
 
 #--network=test-net --ip=172.20.0.6
 echo "启动完成."
--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
-nohup java -jar template.jar --spring.profiles.active=dev --spring.datasource.password=xxxxx \
+nohup java -jar template.jar --spring.profiles.active=test --spring.datasource.password=xxxxx \
 --spring.redis.password=xxxxx --server.port=8080 >/etc/null 2>&1 &
