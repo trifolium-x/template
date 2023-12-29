75dafd2eaeb7ace486ac4cceae803d70f5e4455f
trifolium.wang <trifolium.wang@foxmail.com>
2023-12-29 09:29:37 +0800

    :art: 添加启动标记，方便监控容器中启动事件

--- services/template-service/src/main/java/com/example/template/service/template/TemplateApplication.java
+++ services/template-service/src/main/java/com/example/template/service/template/TemplateApplication.java
@@ -1,14 +1,19 @@
 package com.example.template.service.template;
 
+import com.example.template.common.util.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@Slf4j
 @SpringBootApplication(scanBasePackages = "com.example.template", proxyBeanMethods = false)
 @MapperScan(basePackages = "com.example.template.repo.mapper")
 public class TemplateApplication {
 
     public static void main(String[] args) {
+
+        log.info("应用启动标记--date:{}", DateUtil.getDateStr());
         SpringApplication.run(TemplateApplication.class, args);
     }
 
